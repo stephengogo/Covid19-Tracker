863db7355a547376503e01a310ee6e203246d459
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-06 02:40:47 -0700

    Menu Item with country

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core";
+
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([
+    'USA', 'UK', 'India'
+  ]);
+  // STATE = How to write a variable in REACT
+  // https://disease.sh/v3/covid-19/countries
+  
   return (
     <div className="app">
-      <h1>Lets build a COVID 19 Tracker</h1>
+      <div className="app_header">
+        <h1>Lets build a COVID 19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc"
+          >
+            {
+              countries.map((country) => (
+                <MenuItem value={country}>{country}</MenuItem>
+
+            ))}
+            {/* Loop through all the countries and show a drop down list of the options */}
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">Yooo</MenuItem> */}
+
+          </Select>
+        </FormControl>
+      </div>
+      
+      {/* Header */}
+      {/* {Title + select input dropdown field} */}
+      {/* {infoBoxes} */}
+      {/* {infoBoxes} */}
+      {/* {infoBoxes} */}
+
+      {/* {Table} */}
+      {/* {Graph} */}
+      {/* {Map} */}
     </div>
   );
 }

714a931cc6783ecfbeeb8679d79501f8b122abfc
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-06 21:42:40 -0700

    Menu selectable and stays selected

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   MenuItem,
   FormControl,
@@ -8,30 +8,44 @@ import {
 import './App.css';
 
 function App() {
-  const [countries, setCountries] = useState([
-    'USA', 'UK', 'India'
-  ]);
-  // STATE = How to write a variable in REACT
-  // https://disease.sh/v3/covid-19/countries
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch (" https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((x) => (
+          {
+            name: x.country, // United States, United Kingdom 
+            value: x.countryInfo.iso2 // UK, USA, FR
+          }));
+          setCountries(countries); // Same as countries.setCountries
+      });
+    };
+
+    getCountriesData();
+  }, []); // anytime something changes [countries]
   
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+
+    console.log('Yooo', countryCode);
+
+    setCountry(countryCode);
+  }
+
   return (
     <div className="app">
       <div className="app_header">
         <h1>Lets build a COVID 19 Tracker</h1>
         <FormControl className="app_dropdown">
-          <Select variant="outlined" value="abc"
-          >
-            {
-              countries.map((country) => (
-                <MenuItem value={country}>{country}</MenuItem>
-
+          <Select variant="outlined" onChange={onCountryChange} value={country} >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
-            {/* Loop through all the countries and show a drop down list of the options */}
-            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Option 2</MenuItem>
-            <MenuItem value="worldwide">Option 3</MenuItem>
-            <MenuItem value="worldwide">Yooo</MenuItem> */}
-
           </Select>
         </FormControl>
       </div>
